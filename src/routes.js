3076a0e466c04f8469a97adf5838db8a4b36235d
Silvio Junior <silvio.junior.souza@gmail.com>
2019-06-25 12:49:10 -0300

    Primeira versão do projeto

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const validate = require('express-validation')
+
+const routes = express.Router()
+
+const authMiddleware = require('./app/middlewares/auth')
+
+const controllers = require('./app/controllers')
+const validators = require('./app/validators')
+
+routes.post('/users', validate(validators.User), controllers.UserController.store)
+routes.post('/sessions', validate(validators.Session), controllers.SessionController.store)
+
+routes.use(authMiddleware)
+
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
+
+
+module.exports = routes
---
+++ src/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production'
+
+    this.database()
+    this.middlewares()
+    this.routes()
+  }
+
+  database () {
+    mongoose.connect('mongodb+srv://deploy:102030@cluster0-ipdds.mongodb.net/market?retryWrites=true&w=majority', {useCreateIndex : true, useNewUrlParser: true, useFindAndModify: false})
+
+  }
+
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
+  }
+}
+
+module.exports = new App().express
